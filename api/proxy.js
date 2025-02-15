575192f41fa0d983b531928081789f7532b87f28
Hanson-zhy <157664656+Hanson-zhy@users.noreply.github.com>
2025-02-16 01:28:31 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://miserable-malina-hanson-skipcnwall-ee19a0b4.koyeb.app/";//your website url
+  let target = "https://accessible-dacey-hanson-zhy-1aad55ca.koyeb.app";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
